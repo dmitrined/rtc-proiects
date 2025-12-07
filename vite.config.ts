5db0e736153d70787a8083f2263fbdc317e9d357
dmitrined <dm.nedioglo@gmail.com>
2025-12-07 09:06:11 +0100

    07.12

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
+  build: {
+    // Устанавливаем новый лимит, например, 1000 КБ (1MB)
+    chunkSizeWarningLimit: 1000, 
+  },
    
 })
